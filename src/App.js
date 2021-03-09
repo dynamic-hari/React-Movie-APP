20a4cdceb090f4aa11524f2372e6eef0ba3c2fc6
Hari Haran K S <78909335+dynamic-hari@users.noreply.github.com>
2021-03-09 23:05:18 +0530

    Updated Commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import MovieDetails from './components/MovieDetails';
-import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Movie from './components/Movie';
+import { BrowserRouter as Router, Route , Redirect} from 'react-router-dom';
 
 function App() {
 
   return (
     <Router>
       <>
+        <Header />
         <Route exact path="/" component={() => (<Redirect to='/moviecart' />)} />
-        <MovieDetails />
+        <Route exact path="/moviecart" component={Movie} />
+        <Route path="/moviecart/:title/details" component={MovieDetails} />
       </>
     </Router>
   );
